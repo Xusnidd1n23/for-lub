5195d74af2d7efbcf95dff2a865a70868f58d422
Xusniddin <stayleruzb@gmail.com>
2025-11-17 18:27:41 +0500

    Add files via upload

---
+++ 17.py
@@ -0,0 +1,36 @@
+#1-masala
+ism = " Maqsadbek"
+for x in range(1, len(ism)+1):
+    print(f"{x}: {ism[x-0]}")
+
+#2-masala
+dostlar = ("Hikmatjon", "Yashnarbek", "Alisher","Zahrulloh","Temurbek","Maqsadbek","Behruzbek","Aslbek","Foziljon","Ulug'bek")
+for dost in dostlar:
+    if dost in "Hikmatjon":
+        print(f"{dost} - Salom qalaysan!")
+    elif dost in "Yashnarbek":
+        print(f"{dost} - Salam alekum jo'ra")
+    elif dost in "Alisher":
+        print(f"{dost} - Assalomu aleykum jigar")
+    elif dost in "Zahrulloh":
+        print(f"{dost} - Assalomu Aleykum Va Rahmatullohu Va Barakattuh qori aka")
+    elif dost in "Temurbek":
+        print(f"{dost} - Na gap")
+    elif dost in "Maqsadbek":
+        print(f"{dost} - привет")
+    elif dost in "Behruzbek":
+        print(f"{dost} - Hello")
+    elif dost in "Aslbek":
+        print(f"{dost} - Oydinma")
+    elif dost in "Foziljon":
+        print(f"{dost} - Nichiksan")
+    elif dost in "Ulug'bek":
+        print(f"{dost} - salomlar salomlar")
+
+
+
+
+
+
+
+
